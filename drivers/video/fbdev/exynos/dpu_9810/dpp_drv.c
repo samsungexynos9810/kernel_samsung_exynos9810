18076ea0d4f4ca6b03a1d8715f5890bdffe8c10b
xxmustafacooTR <mustafa.gokmen2004@gmail.com>
2022-03-15 22:51:19 +0300

    exynos9810: import G960FXXUHFVB4 changes

--- drivers/video/fbdev/exynos/dpu_9810/dpp_drv.c
+++ drivers/video/fbdev/exynos/dpu_9810/dpp_drv.c
@@ -631,7 +631,7 @@ static int dpp_check_limitation(struct dpp_device *dpp, struct dpp_params_info *
 	}
 
 	/* HDR channel limitation */
-	if ((p->hdr != DPP_HDR_OFF) && (p->rot > DPP_ROT_180)) {
+	if ((p->hdr != DPP_HDR_OFF) && p->rot) {
 		dpp_err("Not support [HDR+ROTATION] at the same time in DPP%d\n",
 			dpp->id);
 		return -EINVAL;
